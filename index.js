8fc3f28cc9392c8c5cdc98214ebb28d51941ae4e
Deepak Prajapat <94034668+deepak411prajapat@users.noreply.github.com>
2024-08-05 13:49:25 +0530

    index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://friendchatting.netlify.app/",
+    origin: "https://friendchatting.netlify.app",
     methods: ["GET", "POST"],
   },
 });
